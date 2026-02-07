80f50b3da466272a3269558c557ac61c690a0428
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-07 20:00:20 +0600

    add CRUD operations to speciality module

--- src/app/modules/speciality/speciality.controller.ts
+++ src/app/modules/speciality/speciality.controller.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 import {specialityService} from "./speciality.service";
+import {prisma} from "../../../lib/prisma";
 
 const createSpeciality = async (req: Request, res: Response) => {
     try {
@@ -12,7 +13,7 @@ const createSpeciality = async (req: Request, res: Response) => {
             data: result,
             message: "Speciality created successfully"
         })
-    } catch (error:any) {
+    } catch (error: any) {
         res.status(400).json({
             success: false,
             message: error.message
@@ -21,6 +22,46 @@ const createSpeciality = async (req: Request, res: Response) => {
     }
 }
 
+
+const getAllSpecialities = async (req: Request, res: Response) => {
+    try {
+        const specialities = await specialityService.getAllSpecialities();
+        res.status(200).json({
+            success: true,
+            data: specialities,
+            message: "Specialities fetched successfully"
+        })
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+                success: false,
+                message: error.message
+            }
+        )
+    }
+}
+
+
+const deleteSpeciality = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        const result = await specialityService.deleteSpeciality(id as string);
+        res.status(200).json({
+            success: true,
+            data: result,
+            message: "Speciality deleted successfully"
+        })
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+                success: false,
+                message: error.message
+            }
+        )
+    }
+}
 export const specialityController = {
-    createSpeciality
+    createSpeciality,
+    getAllSpecialities,
+    deleteSpeciality
 }
--- src/app/modules/speciality/speciality.service.ts
+++ src/app/modules/speciality/speciality.service.ts
@@ -1,14 +1,29 @@
-
 import {prisma} from "../../../lib/prisma";
+import {speciality} from "../../../generated/prisma/client";
 
 
-const createSpeciality = async (payload :Speciality):Promise<Speciality>=> {
+const createSpeciality = async (payload: speciality): Promise<speciality> => {
     const speciality = await prisma.speciality.create({
         data: payload
     })
     return speciality;
 }
 
+
+const getAllSpecialities = async (): Promise<speciality[]> => {
+    const specialities = await prisma.speciality.findMany();
+    return specialities;
+}
+
+const deleteSpeciality = async (id: string): Promise<speciality> => {
+
+    const speciality = await prisma.speciality.delete({
+        where: {id}
+    })
+    return speciality;
+}
 export const specialityService = {
-    createSpeciality
+    createSpeciality,
+    getAllSpecialities,
+    deleteSpeciality
 }

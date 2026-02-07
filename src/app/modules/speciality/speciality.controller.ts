fde58dad4061ef3875e8129a2fa96e0ec552131a
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-07 21:58:26 +0600

    refactor speciality controller to use centralized response handler

--- src/app/modules/speciality/speciality.controller.ts
+++ src/app/modules/speciality/speciality.controller.ts
@@ -2,41 +2,41 @@ import {NextFunction, Request, RequestHandler, Response} from "express";
 import {specialityService} from "./speciality.service";
 import {prisma} from "../../../lib/prisma";
 import {catchAsync} from "../../shared/catchAsync";
+import {sendResponse} from "../../shared/sendResponse";
 
-const createSpeciality =catchAsync(async (req: Request, res: Response) => {
+
+const createSpeciality = catchAsync(async (req: Request, res: Response) => {
     const payload = req.body;
     const result = await specialityService.createSpeciality(payload);
-    res.status(201).json({
+    sendResponse(res, {
+        httpStatusCode: 201,
         success: true,
         data: result,
         message: "Speciality created successfully"
     })
 })
 
+const getAllSpecialities = catchAsync(async (req: Request, res: Response) => {
+    const result = await specialityService.getAllSpecialities();
+    sendResponse(res, {
+        httpStatusCode: 200,
+        success: true,
+        data: result,
+        message: "Specialities fetched successfully"
+    })
+})
 
-
-
-const getAllSpecialities = catchAsync(
-    async (req:Request,res:Response)=>{
-        const result = await specialityService.getAllSpecialities();
-        res.status(200).json({
-            success: true,
-            data: result,
-            message: "Specialities fetched successfully"
-        })
-    }
-)
-
-
-const deleteSpeciality = catchAsync(async (req:Request,res:Response)=>[p{
+const deleteSpeciality = catchAsync(async (req: Request, res: Response) => {
     const {id} = req.params
     const result = await specialityService.deleteSpeciality(id as string)
-    res.status(200).json({
+    sendResponse(res, {
+        httpStatusCode: 200,
         success: true,
         data: result,
         message: "Speciality deleted successfully"
     })
 })
+
 export const specialityController = {
     createSpeciality,
     getAllSpecialities,

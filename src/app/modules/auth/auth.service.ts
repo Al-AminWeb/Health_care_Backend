404a8cdef681b7747669655b84f2477fd7a8ee17
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-08 21:25:08 +0600

    add user login functionality to authentication module with route, controller, and service implementation

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,13 @@
 
 import {auth} from "../../lib/auth";
+import {UserStatus} from "../../../generated/prisma/enums";
 
 interface IRegisterPatientPayload {
     name: string;
     email: string;
     password: string;
 }
+
 
 
 const registerPatient = async (payload: IRegisterPatientPayload) => {
@@ -27,6 +29,34 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
     return data
 }
 
+interface ILoginUserPayload {
+    email: string;
+    password: string;
+}
+
+const loginUser = async (payload: ILoginUserPayload) => {
+    const { email, password } = payload;
+
+    const data = await auth.api.signInEmail({
+        body: {
+            email,
+            password,
+        }
+    })
+
+    if (data.user.status === UserStatus.BLOCKED) {
+        throw new Error("User is blocked");
+    }
+
+    if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
+        throw new Error("User is deleted");
+    }
+
+    return data;
+
+}
+
 export const authService = {
     registerPatient
+    ,loginUser
 }

041b2763c8a1adee679d4cadfd2b43ea215027a1
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-11 23:28:37 +0600

    integrate enhanced global error handling with Zod validation support, define error interfaces, and implement helper for processing Zod errors

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,19 +1,52 @@
 import {NextFunction, Request, Response} from "express";
 import {envVars} from "../config/env";
 import status from "http-status";
+import z from "zod";
+import {TErrorResponse, TErrorSources} from "../interfaces/error.interfaces";
+import {handleZodError} from "../errorHelpers/handleZodError";
+
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     if (envVars.NODE_ENV === "development") console.log("error from global error handler:", err)
 
-    const statusCode: number = status.INTERNAL_SERVER_ERROR;
+    const errorSource: TErrorSources[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR;
+    let message: string = "Internal server error";
 
-    const message: string = "Internal server error";
 
+    if (err instanceof z.ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number
+        message = simplifiedError.message
+        errorSources = [...simplifiedError.errorSources]
+        stack = err.stack;
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    } else if (err instanceof Error) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = err.message
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
 
-    res.status(statusCode).json({
-            success: false,
-            error: err.message,
-            message: message
-        }
-    )
+    const errorResponse: TErrorResponse = {
+        success: false,
+        message: message,
+        errorSources,
+        error: envVars.NODE_ENV === "development" ? err : undefined
+    }
+    res.status(statusCode).json(errorResponse)
 }
